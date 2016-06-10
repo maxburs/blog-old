a1fb858a5747939863e5871f70c6971462e0c16b
Max Burson <maxwellburson@gmail.com>
2016-06-10 14:15:40 -0700

    added 100% project link width on small displays

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+var layoutLarge = true;
+var imgLinks;
 window.onload = function() {
-    console.log("test");
+    imgLinks = document.getElementsByClassName("img-link");
+    layoutCheck();
+    window.addEventListener("resize", layoutCheck, false);
+    console.log(imgLinks);
+}
+
+function layoutCheck(){
+    if (layoutLarge && document.body.clientWidth < 450) {
+        layoutLarge = false;
+        for (var i = 0; i < imgLinks.length; i++){
+            imgLinks[i].style.width = "100%";
+        }
+    }
+    else if (!layoutLarge && document.body.clientWidth >= 450) {
+        layoutLarge = true;
+        for (var i = 0; i < imgLinks.length; i++){
+            imgLinks[i].style.width = "50%";
+        }
+    }
 }
