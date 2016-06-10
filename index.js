c27afba5f9b39f2923eb55aa6c2499a150c21965
Max Burson <maxwellburson@gmail.com>
2016-06-10 14:28:37 -0700

    added title wrapping

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-var layoutLarge = true;
+var smallThumbnails = true;
+var wrappedTitle = false;
 var imgLinks;
+var title;
 window.onload = function() {
     imgLinks = document.getElementsByClassName("img-link");
+    title = document.getElementsByTagName("h1")[0];
+    console.log(title);
     layoutCheck();
     window.addEventListener("resize", layoutCheck, false);
     console.log(imgLinks);
 }
 
 function layoutCheck(){
-    if (layoutLarge && document.body.clientWidth < 450) {
-        layoutLarge = false;
+    if (!wrappedTitle && document.body.clientWidth < 550) {
+        title.innerHTML = "Maxwell<br>Burson";
+        wrappedTitle = true;
+    }
+    else if (wrappedTitle && document.body.clientWidth >= 550) {
+        title.innerHTML = "Maxwell Burson";
+        wrappedTitle = false;
+    }
+    if (smallThumbnails && document.body.clientWidth < 450) {
+        smallThumbnails = false;
         for (var i = 0; i < imgLinks.length; i++){
             imgLinks[i].style.width = "100%";
         }
     }
-    else if (!layoutLarge && document.body.clientWidth >= 450) {
-        layoutLarge = true;
+    else if (!smallThumbnails && document.body.clientWidth >= 450) {
+        smallThumbnails = true;
         for (var i = 0; i < imgLinks.length; i++){
             imgLinks[i].style.width = "50%";
         }
