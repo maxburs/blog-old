3547ed7d4b3c7c5aced638ae72c1cdf8d35ff782
Maxwell Burson <maxwellburson@gmail.com>
2016-08-22 15:27:45 -0700

    now compiling with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    "index.css": "src/index.scss"
+                }
+            }
+        },
+        uglify: {
+            build: {
+                src: 'src/index.js',
+                dest: 'index.js'
+            }
+        },
+        watch: {
+            files: ['src/*.js', 'src/*.scss'],
+            tasks: ['uglify', 'sass']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['uglify', 'sass']);
+};
---
+++ src/index.js
@@ -0,0 +1,33 @@
+var smallThumbnails = true;
+var wrappedTitle = false;
+var imgLinks;
+var title;
+window.onload = function() {
+    imgLinks = document.getElementsByClassName("img-link");
+    title = document.getElementsByTagName("h1")[0];
+    //layoutCheck();
+    //window.addEventListener("resize", layoutCheck, false);
+}
+
+function layoutCheck(){
+    if (!wrappedTitle && document.body.clientWidth < 550) {
+        title.innerHTML = "Maxwell<br>Burson";
+        wrappedTitle = true;
+    }
+    else if (wrappedTitle && document.body.clientWidth >= 550) {
+        title.innerHTML = "Maxwell Burson";
+        wrappedTitle = false;
+    }
+    if (smallThumbnails && document.body.clientWidth < 450) {
+        smallThumbnails = false;
+        for (var i = 0; i < imgLinks.length; i++){
+            imgLinks[i].style.width = "100%";
+        }
+    }
+    else if (!smallThumbnails && document.body.clientWidth >= 450) {
+        smallThumbnails = true;
+        for (var i = 0; i < imgLinks.length; i++){
+            imgLinks[i].style.width = "50%";
+        }
+    }
+}
